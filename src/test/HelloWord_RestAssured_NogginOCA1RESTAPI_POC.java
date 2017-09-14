12a35263e5d865d48ef5a9ffc2f75bf1cc7c312f
ssubhan <ssubhan@10.200.25.138>
2017-09-14 13:49:55 +1000

    First Commit

---
+++ src/test/HelloWord_RestAssured_NogginOCA1RESTAPI_POC.java
@@ -0,0 +1,230 @@
+package test;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import java.util.List;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+
+public class HelloWord_RestAssured_NogginOCA1RESTAPI_POC {
+	
+	public static Response response;
+	public static String jsonAsString;
+	
+	//Global variables
+	String sessionID=null;
+	String TermsAckCode=null;
+	
+	@BeforeClass
+	
+	public  void setProxy() 
+	{
+		RestAssured.proxy("proxy.lan.noggin.com.au", 3128);
+		//RestAssured.useRelaxedHTTPSValidation();
+		
+	/*
+		System.setProperty("http.proxyHost", "proxy.lan.noggin.com.au");
+	System.setProperty("http.proxyPort", "3128");
+	System.setProperty("https.proxyHost", "proxy.lan.noggin.com.au");
+	System.setProperty("https.proxyPort", "3128");
+	*/
+	}
+	
+	
+	@Test
+	public void NogginOCARESTTestCreateSession() {
+	     
+ //Initializing payload or API body
+		String APIBody = "{\"Username\":\"sabbir\",\"Password\":\"123test\",\"MobilePIN\":\"\"}"; //e.g.- "{\"key1\":\"value1\",\"key2\":\"value2\"}"
+		  
+	
+// Building request using requestSpecBuilder
+		RequestSpecBuilder builder = new RequestSpecBuilder();
+
+//Setting API's body
+		builder.setBody(APIBody);
+	
+//Setting content type as application/json or application/xml
+		builder.setContentType("application/json; charset=UTF-8");
+
+		RequestSpecification requestSpec = builder.build();	
+		
+//lets print RequestSpecification
+		//System.out.println(requestSpec.head().asString());
+	
+
+//Sending the POST request with 	
+
+				
+				Response response=given()
+						.spec(requestSpec)
+						.header("Accept", "application/json")
+						.when()
+						.post("https://bitnoiseqa.nogginoca.com:443/api/v1/session");
+				
+		
+				
+		//ResponseBody<?> resBobdy=response.body();
+		//print response
+		 response.prettyPeek();
+		//status code 
+		 System.out.println("Status code:"+response.getStatusCode());
+		 //status line
+		 System.out.println("Status line:"+response.statusLine());
+		 
+		 //Header code 
+		 System.out.println("Header Content-Type:"+response.getHeader("Content-Type"));
+		 System.out.println("Header Expires:"+response.getHeader("Expires"));
+		 System.out.println("Header Date:"+response.getHeader("Date"));
+		 
+		 
+		 //Assertions
+		 JsonPath jsonPath = new JsonPath(response.getBody().asString()); //casting  Rest Assured Response to JSonPath object
+		 String stateCode=jsonPath.getString("responsePayloads.StateCode").replace("[", "").replace("]", "").trim();
+		 sessionID=jsonPath.getString("responsePayloads.SessionID").replace("[", "").replace("]", "").trim();//removing start [ and end ] from the string
+		 TermsAckCode=jsonPath.getString("responsePayloads.TermsAckCode").replace("[", "").replace("]", "").trim();
+		 System.out.println("State code from JSon Path "+stateCode);
+		 System.out.println("Session ID from JSon Path "+sessionID);
+		 System.out.println("Terms code from JSon Path "+TermsAckCode);
+		 //List 
+		 
+		 List<Object> responsePayloads = jsonPath.getList("responsePayloads");
+		 System.out.println("size:" +responsePayloads.size());
+		 System.out.println("As tring" +responsePayloads.toString());
+		 
+		 response.
+		 then()
+		 .body("responsePayloads.StateCode",contains("WAIT_TERMS_AND_CONDITIONS"));//using JSON Path
+		 
+						 
+
+	}
+	
+	@Test(dependsOnMethods = { "NogginOCARESTTestCreateSession" })
+	public void NogginOCARESTTestAcceptTermsAndConditions() {
+	     
+ //Initializing payload or API body
+		String APIBody = "{\"TermsAccept\": ["+TermsAckCode+"]}"; //e.g.- "{\"key1\":\"value1\",\"key2\":\"value2\"}"
+		  
+	
+// Building request using requestSpecBuilder
+		RequestSpecBuilder builder = new RequestSpecBuilder();
+
+//Setting API's body
+		builder.setBody(APIBody);
+	
+//Setting content type as application/json or application/xml
+		builder.setContentType("application/json; charset=UTF-8");
+		builder.addHeader("X-Session-ID",sessionID);//adding Session ID to header
+
+		RequestSpecification requestSpec = builder.build();	
+		
+//lets print RequestSpecification
+		//System.out.println(requestSpec.head().asString());
+	
+
+//Sending the POST request with 	
+
+				
+				Response response=given()
+						.spec(requestSpec)
+						.header("Accept", "application/json")
+						.when()
+						.put("https://bitnoiseqa.nogginoca.com:443/api/v1/session");
+				
+		
+				
+		//ResponseBody<?> resBobdy=response.body();
+		//print response
+		 response.prettyPeek();
+		//status code 
+		 System.out.println("Status code:"+response.getStatusCode());
+		 //status line
+		 System.out.println("Status line:"+response.statusLine());
+		
+		 
+		 //Assertions
+
+		 response.
+		 then()
+		 .body("responsePayloads.StateCode",contains("AUTHENTICATED"));	 //checking if session creation is successful 
+		 		 
+
+	}
+	
+	@Test(dependsOnMethods = { "NogginOCARESTTestAcceptTermsAndConditions" })
+	public void NogginOCAREStGetContacts() {
+	     
+ //Initializing payload or API body
+		//String APIBody = "{\"TermsAccept\": ["+TermsAckCode+"]}"; //e.g.- "{\"key1\":\"value1\",\"key2\":\"value2\"}"
+		  
+	
+// Building request using requestSpecBuilder
+		RequestSpecBuilder builder = new RequestSpecBuilder();
+
+//Setting API's body
+		//builder.setBody(APIBody);
+	
+//Setting content type as application/json or application/xml
+		builder.setContentType("application/json; charset=UTF-8");
+		builder.addHeader("X-Session-ID",sessionID);//adding Session ID to header
+
+		RequestSpecification requestSpec = builder.build();	
+		
+//lets print RequestSpecification
+		//System.out.println(requestSpec.head().asString());
+	
+
+//Sending the POST request with 	
+
+				
+				Response response=given()
+						.spec(requestSpec)
+						.header("Accept", "application/json")
+						.when()
+						.get("https://bitnoiseqa.nogginoca.com:443/api/v1/contacts");
+				
+			
+		
+		//print response
+		 response.prettyPeek();
+		//status code 
+		 System.out.println("Status code:"+response.getStatusCode());
+		 //status line
+		 System.out.println("Status line:"+response.statusLine());
+		
+		//Assertions
+
+		 response.
+		 then()
+		 .body("responsePayloads.Name",hasItems("Appium","Contact")); //check if Name array has Appium and Contact
+		 
+		//print Name all contacts
+		JsonPath jsonPath = new JsonPath(response.getBody().asString()); //casting  Rest Assured Response to JSonPath object
+		 List<Object> contactName = jsonPath.getList("responsePayloads.Name");
+		 System.out.println("Contact size:" +contactName.size());
+		 
+		 //for loop
+		 for(int i=0;i<contactName.size();i++){
+			 System.out.println("Contact Name No:"+(i+1)+":"+contactName.get(i).toString());
+		 }
+		 
+		 String nextPageURL=jsonPath.getString("nextPageURL");
+		 System.out.println("nextPageURL:" +nextPageURL);
+		 		 
+
+	}
+
+	
+
+}
---
+++ src/test/RESTHealthDirect.java
@@ -0,0 +1,68 @@
+package test;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import static io.restassured.RestAssured.given;
+
+import io.restassured.RestAssured;
+
+
+
+public class RESTHealthDirect {
+public static void main(String[] args) {
+buildAndExecuteClientAPIReq();
+}
+
+public static void buildAndExecuteClientAPIReq() {
+		String clientLocationSearchUrl = "https://servicefinder.myagedcare.gov.au/api/acg/v1/helpAtHomeCarePackageFinder";
+		String jsonBody = "{\"helpAtHomeCarePackageFinderRequest\":{\"helpAtHomeCarePackageFinderInput\":{\"serviceTypes\":{serviceType\":[\"Home Care Package Level 1\",\"Home Care Package Level 2\",\"Home Care Package Level 3\",\"Home Care Package Level 4\"]},\"clientLocationSearch\":{\"localitySearch\":{\"suburb\":\"SYDNEY\",\"state\":\"NSW\",\"postcode\":\"2000\"}}}}}";
+		
+		JsonPath jsonPath = furnishBodyAndGetResponse(clientLocationSearchUrl, jsonBody);
+		jsonPath.prettyPeek();
+
+}
+
+public static JsonPath furnishBodyAndGetResponse(String searchUrl, String jsonBody) {
+	RestAssured.proxy("proxy.lan.noggin.com.au", 3128);
+	System.out.println(buildAPIRequestSpec().get().asString());
+	Response resp = buildAPIRequestSpec().body(jsonBody).post(searchUrl);
+	resp.prettyPeek();
+	
+	
+	boolean logRestAssured = Boolean.getBoolean("restassured.log.all");
+	
+	ValidatableResponse validatableResp = resp.then();
+	
+	if (logRestAssured) {
+			validatableResp.log().all();
+		}
+
+		validatableResp.statusCode(200);
+		
+		JsonPath jsonPath = resp.jsonPath();
+		
+		return jsonPath;
+}
+
+public static RequestSpecification buildAPIRequestSpec() {
+		boolean logRestAssured = Boolean.getBoolean("restassured.log.all");
+		
+		RequestSpecification apiRequest=given();
+		
+		if (logRestAssured) {
+		apiRequest.log().all();
+		}
+
+		apiRequest.contentType("application/json; charset=UTF-8")
+		.accept("application/json")
+		//.baseUri("https://")
+		//.basePath("servicefinder.myagedcare.gov.au/api/acg/v1/helpAtHomeCarePackageFinder")
+		.header("x-api-key", "7ca4c25771c54ca283c682a185e72277")		
+		.when();
+		
+		return apiRequest;
+}
+
+
+}
